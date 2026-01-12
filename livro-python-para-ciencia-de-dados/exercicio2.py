fix(exercicio2): Complete calculation for highest salary across 3 months

livro-python-para-ciencia-de-dados/exercicio2.py
@@ -23,4 +23,5 @@ print("\n")
 print("Salário Total:")
 print(salary_bonus)
 
-maiorSal3mes
+maiorSal3mes = np.amax(salary_bonus, axis = 1)
+
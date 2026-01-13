feat(exercicio2): Add initial salary calculation script and fix print output This commit introduces `tempCodeRunnerFile.py` containing an initial numpy salary calculation setup and fixes a print statement in `exercicio2.py` to add a newline before printing the maximum salary.

livro-python-para-ciencia-de-dados/tempCodeRunnerFile.py
@@ -33,6 +33,6 @@ maiorSal = np.amax(salary_bonus)                                        #Encontr
 medianaSal = np.median(salary_bonus)
 mediaSal= np.average(salary_bonus)
 
-print("\n" + maiorSal)
+print(maiorSal)
 print(medianaSal)
 print(mediaSal)
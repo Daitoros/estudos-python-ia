b54d0e89630a95db2488f29c5b9966e5c7cf1c89
Daitoros <daviabmedeiros@gmail.com>
2026-01-13 09:30:42 -0300

    fix(exercicio2): Clarify numpy axis 0 comment in salary calculation

--- livro-python-para-ciencia-de-dados/exercicio2.py
+++ livro-python-para-ciencia-de-dados/exercicio2.py
@@ -24,7 +24,7 @@ print("Salário Total:")
 print(salary_bonus)
 
 maiorSalFunc = np.amax(salary_bonus, axis = 1)                  #(axis = 1) Percorre e compara por linha (cada funcionario está em uma linha). no final vai ter um total de valores equivalente ao total de linhas
-maiorSalmes = np.amax(salary_bonus, axis = 0)                   #(axis = 0) Percorre e compara por coluna ()
+maiorSalmes = np.amax(salary_bonus, axis = 0)                   #(axis = 0) Percorre e compara por coluna (os meses estão representados por colunas).
 
 #conjuntoMaior = np.concatenate((maiorSal3mes, maiorSalCol))            não é necessário concatenar aqui
 #maiorSal = np.amax(conjuntoMaior, axis=1)

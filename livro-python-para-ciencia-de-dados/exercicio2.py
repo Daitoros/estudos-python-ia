65f06a0b48743e5341d6da88bbc0f40316e4d73b
Daitoros <daviabmedeiros@gmail.com>
2026-01-13 08:54:44 -0300

    refactor(exercicio2): Comment out unused maximum salary calculation

--- livro-python-para-ciencia-de-dados/exercicio2.py
+++ livro-python-para-ciencia-de-dados/exercicio2.py
@@ -27,7 +27,7 @@ maiorSal3mes = np.amax(salary_bonus, axis = 1)
 maiorSalCol = np.amax(salary_bonus, axis = 0)
 
 #conjuntoMaior = np.concatenate((maiorSal3mes, maiorSalCol))            não é necessário concatenar aqui
-maiorSal = np.amax(conjuntoMaior, axis=1)
+#maiorSal = np.amax(conjuntoMaior, axis=1)
 
 
 

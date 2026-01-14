30f53180a43eee19523e4558db0e4fcb85f4cf1d
Daitoros <daviabmedeiros@gmail.com>
2026-01-14 14:43:42 -0300

    Add(exercicio4): Adicionados outros exemplos de joins

--- livro-python-para-ciencia-de-dados/exercicio4-joins.py
+++ livro-python-para-ciencia-de-dados/exercicio4-joins.py
@@ -20,10 +20,49 @@ df = pd.concat([nomes,emails,phones], axis = 1)
 # print("\n")
 # print(df)
 
-data = [1000, 2000, 3000]
+data = [1000, 2000]
 salario = pd.Series(data, name = 'salario')
-
 df_sal = df.join(salario)
 print("\n")
+print("Left:")
 print(df_sal)
 
+
+#Usado aqui as mesmas variáveis pois é apenas demonstração
+data = [1000, 2000, 3000]
+salario = pd.Series(data, name = 'salario')
+df_sal = df.join(salario)
+print("\n")
+print("Left 2:")
+print(df_sal)
+df_sal = df_sal.drop(columns=['salario'])
+
+data = [1000, 2000, 3000, 4000]
+salario = pd.Series(data, name = 'salario')
+df_sal = df.join(salario, how = 'left')
+print("\n")
+print("Left 3:")
+print(df_sal)
+df_sal = df_sal.drop(columns=['salario'])
+
+data = [1000, 2000, 3000, 4000]
+salario = pd.Series(data, name = 'salario')
+df_sal = df.join(salario, how='right')
+print("\n")
+print("Right:")
+print(df_sal)
+df_sal = df_sal.drop(columns=['salario'])
+
+data = [1000, 2000, 3000, 4000]
+df_sal = df.join(salario, how='inner')
+print("\n")
+print("Inner:")
+print(df_sal)
+df_sal = df_sal.drop(columns=['salario'])
+
+data = [1000, 2000, 3000, 4000]
+df_sal = df.join(salario, how='outer')
+print("\n")
+print("Outer")
+print(df_sal)
+df_sal = df_sal.drop(columns=['salario'])

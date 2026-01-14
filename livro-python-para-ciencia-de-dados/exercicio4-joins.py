6847ba5cc1b4b27de3a6be3914078b690c36e176
Daitoros <daviabmedeiros@gmail.com>
2026-01-14 14:24:36 -0300

    Add(exercicio4): Implementando diferentes joins

---
+++ livro-python-para-ciencia-de-dados/exercicio4-joins.py
@@ -0,0 +1,29 @@
+#executando diferentes joins
+
+import pandas as pd
+
+data = ['Davi', 'Duduka', 'Apolo']
+nomes = pd.Series(data, name = 'nomes')
+#print(nomes)
+
+data = ['davi.aires', 'duduka.seixas', 'apolo.seixas']
+emails = pd.Series(data, name = 'emails')
+
+df = pd.concat([nomes,emails], axis = 1)
+# print("\n")
+# print(df)
+
+data = ['2555-01','2555-02', '2555-03']
+phones = pd.Series(data, name = 'telefones')
+
+df = pd.concat([nomes,emails,phones], axis = 1)
+# print("\n")
+# print(df)
+
+data = [1000, 2000, 3000]
+salario = pd.Series(data, name = 'salario')
+
+df_sal = df.join(salario)
+print("\n")
+print(df_sal)
+
